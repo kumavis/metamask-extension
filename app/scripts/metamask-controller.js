d3ff876fcf952017b95167a062795fe3536f580f
bitpshr <mail@bitpshr.net>
2018-06-04 21:20:19 -0400

    Remove BlacklistController

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -43,11 +43,11 @@ const seedPhraseVerifier = require('./lib/seed-phrase-verifier')
 const cleanErrorStack = require('./lib/cleanErrorStack')
 const log = require('loglevel')
 
-const AddressBookController = require('@metamask/gaba/AddressBookController').default
-const CurrencyRateController = require('@metamask/gaba/CurrencyRateController').default
-const NetworkStatusController = require('@metamask/gaba/NetworkStatusController').default
-const PhishingController = require('@metamask/gaba/PhishingController').default
-const TokenRatesController = require('@metamask/gaba/TokenRatesController').default
+const AddressBookController = require('gaba/AddressBookController').default
+const CurrencyRateController = require('gaba/CurrencyRateController').default
+const NetworkStatusController = require('gaba/NetworkStatusController').default
+const PhishingController = require('gaba/PhishingController').default
+const TokenRatesController = require('gaba/TokenRatesController').default
 
 module.exports = class MetamaskController extends EventEmitter {
 
